5ec15b375a89b08f89e88ad5572b580757f58d4f
0-k9o <framme228@gmail.com>
2026-07-21 15:14:14 +0200

    Rename File

--- Test1/io.h
+++ Test1/io.h
@@ -1,11 +1,7 @@
-//
-// Created by framme on 21.07.2026.
-//
-
 #ifndef FIRSTTEST_IO_H
 #define FIRSTTEST_IO_H
 
 int readNumber();
 int WriteNumber(int a, int b);
 
-#endif //FIRSTTEST_IO_H
+#endif
