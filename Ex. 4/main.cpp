b9ae52100190a3511d5b9dc7e46b0503766777d5
0-k9o <framme228@gmail.com>
2026-07-22 19:38:49 +0200

    New exercise

--- Ex. 4/main.cpp
+++ Ex. 4/main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int writeNumber() {
+    std::cout << "Enter a number: ";
+    int x;
+    std::cin >> x;
+    return x;
+}
+
+bool isEven(int a) {
+    return (a % 2 == 0);
+}
 
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-
-    const auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    int a = writeNumber();
+    if (isEven(a)) std::cout<<"Even";
+    else std::cout<<"Odd";
 }
